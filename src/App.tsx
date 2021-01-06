e56597a927ebad2994282fdd77a04b2ce6de8bc4
Neldin Džeković <neldin.dzeko@gmail.com>
2021-01-06 23:38:32 +0100

    Add Content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import styled from 'styled-components';
 import './App.css';
 import { Accordion } from './components/Accordion';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 128px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      <Container>
+        <div style={{ width: 768 }}>
+          <Accordion>
+            <div style={{ padding: 24 }}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veritatis dolore quibusdam, nemo eveniet doloremque tempora quisquam laborum vitae dolores facere rerum aperiam eos fugiat voluptatibus consequuntur labore ipsum facilis?
+              </p>
+            </div>
+          </Accordion>
+
+          <Accordion>
+            <div style={{ padding: 24 }}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veritatis dolore quibusdam, nemo eveniet doloremque tempora quisquam laborum vitae dolores facere rerum aperiam eos fugiat voluptatibus consequuntur labore ipsum facilis?
+              </p>
+            </div>
+          </Accordion>
+        </div>
+      </Container>
     </div>
   );
 }
--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import styled from 'styled-components';
 
 const Header = styled.div`
@@ -8,12 +9,19 @@ const Header = styled.div`
   background-color: #d5d5d5;
 `;
 
-export const Accordion = () => {
+const Content = styled.div`
+  border: 1px solid #d5d5d5;
+`;
+
+export const Accordion: FC = ({ children }) => {
   return (
     <div>
       <Header>
         <span>Toggle</span>
       </Header>
+      <Content>
+        {children}
+      </Content>
     </div>
   );
 };
