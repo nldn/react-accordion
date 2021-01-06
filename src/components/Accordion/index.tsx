16594430a7dc6ae106f1be0e39f18a980961d17e
Neldin Džeković <neldin.dzeko@gmail.com>
2021-01-06 23:32:38 +0100

    Add Header.

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,7 +1,19 @@
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 64px;
+  background-color: #d5d5d5;
+`;
+
 export const Accordion = () => {
   return (
     <div>
-      Accordion
+      <Header>
+        <span>Toggle</span>
+      </Header>
     </div>
   );
 };
